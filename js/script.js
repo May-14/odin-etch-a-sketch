a001a8204ed0189a9dd453b36b0c06ddeabe31cc
May <26matmoran@gmail.com>
2024-03-22 15:01:52 -0600

    Centered the grid, and placed it inside a div

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const body = document.querySelector("body");
+const gridContainer = document.querySelector("div");
 
 for (let i = 0; i <16; i++) {
     const container = document.createElement("div");
-    container.classList.add("container");
+    container.classList.add("row-container");
     for (let i = 0; i <16; i++) {
         const div = document.createElement("div");
         div.classList.add("grid");
         container.appendChild(div);
     }
-    body.appendChild(container);
+    gridContainer.appendChild(container);
 }
