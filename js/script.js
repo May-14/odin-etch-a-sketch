42b84f20a9a328619228b1df5dc2a6b875c9c7b7
May <26matmoran@gmail.com>
2024-03-22 17:27:23 -0600

    Simplify code and add border around grid

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
 let gridContainer = document.createElement("div");
 gridContainer.classList.add("full-grid-container");
 let numberOfSquaresPerSide = 16;
+let passedOnce = false;
 const body = document.querySelector("body")
 const button = document.querySelector("button");
 button.insertAdjacentElement("beforebegin", gridContainer)
 
-for (let i = 0; i <numberOfSquaresPerSide; i++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("row-container");
-    for (let i = 0; i <numberOfSquaresPerSide; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid");
-        div.setAttribute("style", `width: ${480/numberOfSquaresPerSide}px; height: ${480/numberOfSquaresPerSide}px;`);
-        rowContainer.appendChild(div);
-        div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = "black"
-        })
-    }
-    gridContainer.appendChild(rowContainer);
-}
 
 function makeNewGrid() {
     body.removeChild(gridContainer)
     gridContainer = document.createElement("div");
     gridContainer.classList.add("full-grid-container");
     button.insertAdjacentElement("beforebegin", gridContainer)
-    numberOfSquaresPerSide = prompt("Enter number of squares per side of grid.")
+    if (passedOnce === true) {
+        numberOfSquaresPerSide = prompt("Enter number of squares per side of grid.");
+    }
+    passedOnce = true;
     if (numberOfSquaresPerSide === "" || numberOfSquaresPerSide === null) {
         numberOfSquaresPerSide = 16;
     }
@@ -52,3 +42,5 @@ function makeNewGrid() {
         gridContainer.appendChild(rowContainer);
     }
 }
+
+makeNewGrid()
