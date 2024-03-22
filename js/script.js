47aa3bb2433e2c882ce6b2f0a5df2e4cbe507d09
May <26matmoran@gmail.com>
2024-03-22 14:54:40 -0600

    Add grid class to grid divs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const body = document.querySelector("body");
 
 for (let i = 0; i <16; i++) {
-    const container = document.createElement("div")
-    container.classList.add("container")
+    const container = document.createElement("div");
+    container.classList.add("container");
     for (let i = 0; i <16; i++) {
         const div = document.createElement("div");
+        div.classList.add("grid");
         container.appendChild(div);
     }
     body.appendChild(container);
